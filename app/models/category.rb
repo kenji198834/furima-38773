14f445531086331551b2cb8e393b6e132731df1e
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-15 21:32:45 +0900

    Categoryモデルのアソシエーションの設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,4 +12,8 @@ class Category < ActiveHash::Base
     { id:10, item_status: 'ハンドメイド' },
     { id:11, item_status: 'その他' }
 ]
+
+include ActiveHash::Associations
+has_many :items
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
 
   validates :user_id, presence: true
