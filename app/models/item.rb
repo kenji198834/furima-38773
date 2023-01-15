b4a7186780f6f83c605c632524483745124df8b9
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-15 20:02:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  
+  validates :user_id, presence: true
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :item_status_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    presence: { message: "can't be blank" }
 end
