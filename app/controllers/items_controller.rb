11081b990ee5c3e05ae648feafdb1e95f044fb75
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-15 18:51:08 +0900

    保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :items_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price)
+  end
+
 end
