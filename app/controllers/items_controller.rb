2a949efa6f1e8e9d968bd209e7ab04cfce671098
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-16 16:38:07 +0900

    プルダウン機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :items_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price)
+    params.require(:item).permit(:image, :name, :description, :category_id, :items_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   has_one_attached :image
   belongs_to :user
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :item_status
   belongs_to :shipping_cost
