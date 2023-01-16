f61141a6640f9f4819a7a82cfdb9604b24c97ff9
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-16 10:38:07 +0900

    Categoryモデルの編集とitem_statusモデルにActiveHash ;:Baseを継承

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,16 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id:1, item_status: '---' },
-    { id:2, item_status: 'レディース' },
-    { id:3, item_status: 'メンズ' },
-    { id:4, item_status: 'ベビー・キッズ' },
-    { id:5, item_status: 'インテリア・住まい・小物' },
-    { id:6, item_status: '本・音楽・ゲーム' },
-    { id:7, item_status: 'おもちゃ・ホビー・グッズ' },
-    { id:8, item_status: '家電・スマホ・カメラ' },
-    { id:9, item_status: 'スポーツ・レジャー' },
-    { id:10, item_status: 'ハンドメイド' },
-    { id:11, item_status: 'その他' }
+    { id:1, category: '---' },
+    { id:2, category: 'レディース' },
+    { id:3, category: 'メンズ' },
+    { id:4, category: 'ベビー・キッズ' },
+    { id:5, category: 'インテリア・住まい・小物' },
+    { id:6, category: '本・音楽・ゲーム' },
+    { id:7, category: 'おもちゃ・ホビー・グッズ' },
+    { id:8, category: '家電・スマホ・カメラ' },
+    { id:9, category: 'スポーツ・レジャー' },
+    { id:10, category: 'ハンドメイド' },
+    { id:11, category: 'その他' }
 ]
 
 include ActiveHash::Associations
