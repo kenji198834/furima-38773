af9f5aef4f4f7722efa06399c70cc52faa6b3a73
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-17 21:52:33 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,30 +9,6 @@ RSpec.describe Item, type: :model do
       it '全ての入力事項が存在すれば登録できる' do
         expect(@item).to be_valid
       end
-      it 'カテゴリーが「---」以外であれば登録できる' do
-        @item.category_id = 2
-        expect(@item).to be_valid
-      end
-      it '商品の状態が「---」以外であれば登録できる' do
-        @item.item_status_id = 2
-        expect(@item).to be_valid
-      end
-      it '発送元の地域が「---」以外であれば登録できる' do
-        @item.prefecture_id = 2
-        expect(@item).to be_valid
-      end
-      it '配送料の負担が「---」以外であれば登録できる' do
-        @item.shipping_cost_id = 2
-        expect(@item).to be_valid
-      end
-      it '発送までの日数が「---」以外であれば登録できる' do
-        @item.shipping_date_id = 2
-        expect(@item).to be_valid
-      end
-      it '価格が半角数字でかつ300円から~9,999,999円であれば登録できる' do
-        @item.price = 300
-        expect(@item).to be_valid
-      end
     end
 
     context '出品登録ができないとき' do
