29f391e3a1259c4c9f03a9c2293584876f1e04f7
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-19 15:49:02 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -36,6 +36,13 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def destroy
+    if @item.user_id != current.user_id
+      @item.destroy
+      redirect_to root_path
+    end
   end
   
   private
