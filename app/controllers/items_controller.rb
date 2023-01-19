2ad172152858a65cabe388db0b87818ce3ed9543
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-19 17:53:48 +0900

    2回目の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,9 +39,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id != current_user
-    @item.destroy
-    redirect_to root_path
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
   
