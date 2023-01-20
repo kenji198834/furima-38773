7e691d875ff2203f4af17273249fe1290494a583
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-20 20:23:24 +0900

    購入の保存機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,9 +7,9 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
-      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -20,6 +20,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token:params[:token])
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token:params[:token])
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -11,7 +11,7 @@ class OrderForm
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-    validates :token
+   
   end
 
   def save
