b3742a8efb05d57756da54c56237c63fc787f193
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-20 17:55:07 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,25 +2,23 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-  end
-
-  def new
     @order_form = OrderForm.new
   end
 
   def create
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :building_name, :phone_number).merge(user_id: current_user.id)
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token:params[:token])
   end
 end
