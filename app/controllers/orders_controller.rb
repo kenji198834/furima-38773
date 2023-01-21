70264633d572c802c8d16522e5f1091adcc1aec6
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-21 14:41:37 +0900

    ログアウト状態の場合は、商品購入ページに遷移しようとすると、ログインページに戻るように記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
   before_action :non_purchased_item, only: [:index, :create]
 
   def index
